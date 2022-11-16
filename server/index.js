24fcee2b1ab1a82c5dc7dbef167cac7b38f46bd9
bertwinkeller <bertwinkeller@gmail.com>
2022-11-15 18:46:54 -0800

    server set up

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+
+const app = express()
+
+
+app.use(bodyParser.json({ limit: "30mb", extendedd: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended :true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://bertwinkeller:Cherrycreek15!@memories.lnuzilo.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+
+async function connect() {
+    try {
+        await mongoose.connect(CONNECTION_URL)
+        console.log('Connected to MongoDB')
+    }catch(error){
+        console.error(error)
+    }
+}
+ 
+    connect()
+    
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+   // .catch((error) => console.log(error.message))
+
+
